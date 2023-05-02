9de645cc0ec7c06868f1e03638a5dcad50dbfd2d
Wander06 <wandersonpsilva7@gmail.com>
2023-05-02 16:27:35 -0300

    funcao-historico-add

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
+var historico = []
+
+function verHistorico(){
+    var primo = document.getElementById("primo");
+    console.log(historico)
+    primo.innerHTML = historico[historico.length - 1]
+}
+
+
 function inserirNumero(numero){
     var display = document.getElementById("input")
     if(numero){
         display.value = display.value + numero
         console.log(numero)
     }
+    limparPrimo()
 }
 
 function numeroPrimo(){
-    var display = document.getElementById("input").value;
+    var display = document.getElementById("input");
+    var primo = document.getElementById("primo");
 
-    if(display == 2){
-        alert("É primo")
+    if(display.value == 2 || display.value == 3 || display.value == 5 || display.value == 7){
+        primo.innerHTML = "Primo"
     }
-    if(display == )
+    else if(display.value % 2 >= 1 && display.value % 3 >= 1 && display.value % 5 >= 1 && display.value % 7 >= 1 ){
+        primo.innerHTML = "Primo"
+    }
+    else{
+        primo.innerHTML = "Não é primo"
+    }
+    
 }
+
+function limparPrimo(){
+    var primo = document.getElementById("primo");
+
+    primo.innerHTML = ""
+}
+
 
 function raiz(){
     var display = document.getElementById("input");
 
-    display.value = "√" + Math.sqrt(display.value)
+    display.value = Math.sqrt(display.value)
     console.log(display)
 }
 
 function limpar(){
     var display = document.getElementById("input")
     display.value = ""
+
+    limparPrimo()
 }
 
 function resultado(){
     var display = document.getElementById("input")
     display.value = eval(display.value)
+
+    historico.push(display.value)
 }
